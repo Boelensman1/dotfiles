2856d12fdfe6470bc35caa5ebf46828ee2a467be
Boelensman1 <wigger.boelens@gmail.com>
2015-11-28 16:21:07 +0100

    added nvim configuration
    Right now just a copy of the vim config
    
    Signed-off-by: Boelensman1 <wigger.boelens@gmail.com>

---
+++ nvim/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# install vundle
+DIRECTORY=~/.dotfiles/vim/bundle/Vundle.vim
+if [ ! -d "$DIRECTORY" ]; then
+    git clone git://github.com/gmarik/vundle.git $DIRECTORY
+fi
+
+# install all other plugins
+vim +PluginInstall! +qall
+
+# compile youcomplete me
+echo "Do you want to compile YouCompleteMe (y/n)?"
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    cd ~/.dotfiles/vim/bundle/YouCompleteMe
+    ./install.py --clang-completer
+fi
