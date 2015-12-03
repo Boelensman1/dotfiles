7a8fd051dc8a91c8dde131400486ebe1592f2ee8
Boelensman1 <me@wiggerboelens.com>
2015-12-03 10:41:46 +0100

    Merged install script of vim and nvim
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- vim+nvim/install.sh
+++ vim+nvim/install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 # install vundle
-DIRECTORY=~/.dotfiles/vim/autoload/plug.vim
+DIRECTORY=~/.dotfiles/vim+nvim/autoload/plug.vim
 if [ ! -d "$DIRECTORY" ]; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo ~/.dotfiles/vim+nvim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # install all other plugins
 vim +PlugInstall! +qall
+nvim +PlugInstall! +qall
